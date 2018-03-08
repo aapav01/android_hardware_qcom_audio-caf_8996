baa584a8ee10d7f47422881c283ebe4f653376a5
Apavayan <info@apavayan.com>
2018-03-08 18:19:56 +0530

    hal: voice_ext: msim: Switched to new start & stop call function
    Change-Id: Ic1317dffd28ae41ac33fec19f745bcaf2105f80c
    Signed-off-by: Apavayan <info@apavayan.com>

--- hal/voice_extn/msim_voice_extn.c
+++ hal/voice_extn/msim_voice_extn.c
@@ -21,6 +21,7 @@
 #include <errno.h>
 #include <cutils/log.h>
 #include <cutils/str_parms.h>
+#include <cutils/properties.h>
 
 #include "audio_hw.h"
 #include "voice.h"
@@ -45,8 +46,8 @@
 
 #define VOICE2_SESS_IDX (VOICE_SESS_IDX + 1)
 
-extern int start_call(struct audio_device *adev, audio_usecase_t usecase_id);
-extern int stop_call(struct audio_device *adev, audio_usecase_t usecase_id);
+extern int voice_start_usecase(struct audio_device *adev, audio_usecase_t usecase_id);
+extern int voice_stop_usecase(struct audio_device *adev, audio_usecase_t usecase_id);
 
 static int msim_phone_type = 1;
 
@@ -55,7 +56,7 @@ int msim_voice_extn_start_call(struct audio_device *adev)
     audio_usecase_t usecase_id;
 
     usecase_id = msim_phone_type == 1 ? USECASE_VOICE_CALL : USECASE_VOICE2_CALL;
-    return start_call(adev, usecase_id);
+    return voice_start_usecase(adev, usecase_id);
 }
 
 int msim_voice_extn_stop_call(struct audio_device *adev)
@@ -63,7 +64,7 @@ int msim_voice_extn_stop_call(struct audio_device *adev)
     audio_usecase_t usecase_id;
 
     usecase_id = msim_phone_type == 1 ? USECASE_VOICE_CALL : USECASE_VOICE2_CALL;
-    return stop_call(adev, usecase_id);
+    return voice_stop_usecase(adev, usecase_id);
 }
 
 int msim_voice_extn_get_session_from_use_case(struct audio_device *adev,
